6fe3b1774aa143b73072d2a3aafa5c4777e7ae47
edlingao <sonido244@gmail.com>
2022-06-15 11:53:04 -0500

    fix(HMR): Fixing hot module reload on local docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-npm i
 
 npm rebuild esbuild
-
-npm run build && rm -fr /var/www/html/* && cp -r dist/* /var/www/html/
 
 exec "$@"
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {    // <-- this object is added
-    port: 3000,
-  },
-  plugins: [react()]
+  plugins: [react()],
 })
